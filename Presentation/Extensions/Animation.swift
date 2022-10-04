7000e1737d94a24338e6477a88a225f4b8cde9f9
Gabriel Xavier <gabrielx4000@hotmail.com>
2022-10-04 14:20:45 -0300

    Add Color Token

---
+++ Presentation/Extensions/Animation.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Lottie
+
+extension Animation {
+    
+    static var disconnect: Animation? {
+        return Animation.named("90478-disconnect", bundle: Bundle(for: Animation.self))
+    }
+    
+}
